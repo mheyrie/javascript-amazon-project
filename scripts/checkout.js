97dbb9912c6593598b2f355f3e1e3a6a9bd35d2b
mheyrie <omota4fidelity@gmail.com>
2024-03-23 16:41:55 +0100

    Made delete button responsive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach((cartItem) => {
     });
 
     cartSumaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
             Delivery date: Tuesday, June 21
             </div>
@@ -102,7 +102,10 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     link.addEventListener('click', () => {
         const productId = link.dataset.productId
         removeFromCart(productId)
-        // console.log(cart)
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`)
+        container.remove()
+        console.log(container)
     })
 });
 
